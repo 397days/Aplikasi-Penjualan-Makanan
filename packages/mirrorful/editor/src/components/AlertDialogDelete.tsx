e5fdcbea558b889e3f7a419235625fa422f4d658
397days <122672783+397days@users.noreply.github.com>
2022-06-29 06:52:34 +0000

    Fixes for Linux

--- packages/mirrorful/editor/src/components/AlertDialogDelete.tsx
+++ packages/mirrorful/editor/src/components/AlertDialogDelete.tsx
@@ -28,4 +28,27 @@ export function AlertDialogDelete({
       leastDestructiveRef={cancelRef}
       onClose={onClose}
     >
-      <AlertDialogOverla
+      <AlertDialogOverlay>
+        <AlertDialogContent>
+          <AlertDialogHeader fontSize="lg" fontWeight="bold">
+            Are you sure?
+          </AlertDialogHeader>
+
+          <AlertDialogBody>
+            Are you sure you want to delete {`"${tokenName}"`}? You {`can't`}{' '}
+            undo this action.
+          </AlertDialogBody>
+
+          <AlertDialogFooter>
+            <Button ref={cancelRef} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme="red" onClick={onDelete} ml={3}>
+              Delete
+            </Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialogOverlay>
+    </AlertDialog>
+  )
+}
