4d11b8a4fc65b04c8ef4ea88ecba46fb473b7a47
397days <122672783+397days@users.noreply.github.com>
2022-07-06 10:01:26 +0000

    Revert "update readme"

--- packages/mirrorful/editor/src/components/ExportSuccessModal.tsx
+++ packages/mirrorful/editor/src/components/ExportSuccessModal.tsx
@@ -6,4 +6,97 @@ import {
   ModalFooter,
   ModalBody,
   ModalCloseButton,
- 
+  Text,
+  Button,
+  Box,
+  Icon,
+  Code,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanels,
+  TabPanel,
+  Link,
+} from '@chakra-ui/react'
+import { FiCheckCircle } from 'react-icons/fi'
+import 'highlight.js/styles/atom-one-dark.css'
+import { CodePreview } from './CodePreview'
+
+export function ExportSuccessModal({
+  isOpen,
+  onClose,
+}: {
+  isOpen: boolean
+  onClose: () => void
+}) {
+  return (
+    <Modal size="lg" isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>
+          <Box css={{ display: 'flex', alignItems: 'center' }}>
+            <Icon
+              as={FiCheckCircle}
+              color="green.400"
+              css={{ marginRight: 8 }}
+            />
+            Your Tokens are Ready!
+          </Box>
+        </ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Text css={{ marginBottom: '24px' }}>
+            Your tokens can now be imported by your app.
+          </Text>
+          <Tabs>
+            <TabList>
+              <Tab>CSS / SCSS</Tab>
+              <Tab>Javascript / Typescript</Tab>
+              <Tab>Tailwind</Tab>
+            </TabList>
+
+            <TabPanels>
+              <TabPanel>
+                <Text css={{ marginBottom: 8 }}>
+                  <span style={{ fontWeight: 'bold' }}>1.</span> Import{' '}
+                  <Code>theme.css</Code> (actual path may vary. You can
+                  reference the <Code>.mirrorful</Code> folder in the root of
+                  your project)
+                </Text>
+                <CodePreview
+                  language="javascript"
+                  textClass="code-snippet"
+                  text={`import './.mirrorful/theme.css'`}
+                />
+                <Text css={{ marginTop: 12, marginBottom: 8 }}>
+                  <span style={{ fontWeight: 'bold' }}>2.</span> Your CSS
+                  Variables can now be accessed anywhere in your app!
+                </Text>
+                <CodePreview
+                  language="css"
+                  textClass="code-snippet"
+                  text={`.primary-button {\n    background-color: var(--color-primary);\n}\n\n.primary-button:hover {\n    background-color: var(--color-primary-hover);\n}`}
+                />
+              </TabPanel>
+              <TabPanel>
+                <Text css={{ marginBottom: 8 }}>
+                  <span style={{ fontWeight: 'bold' }}>1.</span> Import{' '}
+                  <Code>Tokens</Code> (actual path may vary. You can reference
+                  the <Code>.mirrorful</Code> folder in the root of your
+                  project)
+                </Text>
+
+                <CodePreview
+                  language="javascript"
+                  textClass="code-snippet"
+                  text={`import { Tokens } from './.mirrorful/theme'`}
+                />
+
+                <Text css={{ marginTop: 12, marginBottom: 8 }}>
+                  <span style={{ fontWeight: 'bold' }}>2.</span> Use your tokens
+                  anywhere as constants!
+                </Text>
+                <CodePreview
+                  language="javascript"
+                  textClass="code-snippet"
+  
