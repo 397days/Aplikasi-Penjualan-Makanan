a886e2a115403cdd7a39e7d371867330256a48d7
397days <122672783+397days@users.noreply.github.com>
2022-07-06 12:48:45 +0000

    Hints

--- packages/mirrorful/editor/src/components/ExportSuccessModal.tsx
+++ packages/mirrorful/editor/src/components/ExportSuccessModal.tsx
@@ -99,4 +99,58 @@ export function ExportSuccessModal({
                 <CodePreview
                   language="javascript"
                   textClass="code-snippet"
-  
+                  text={`<button\n   style={{ backgroundColor: Tokens.primary.base}}\n> Click here\n</button>`}
+                />
+              </TabPanel>
+              <TabPanel>
+                <Text css={{ marginBottom: 8 }}>
+                  <span style={{ fontWeight: 'bold' }}>1.</span> Import{' '}
+                  <Code>theme_cjs.js</Code> in <Code>tailwind.config.js</Code>{' '}
+                  (actual path may vary. You can reference the{' '}
+                  <Code>.mirrorful</Code> folder in the root of your project)
+                </Text>
+
+                <CodePreview
+                  language="javascript"
+                  textClass="code-snippet"
+                  text={`const { Tokens } = require('./.mirrorful/theme_cjs.js')`}
+                />
+
+                <Text css={{ marginTop: 12, marginBottom: 8 }}>
+                  <span style={{ fontWeight: 'bold' }}>2.</span> Extend the
+                  tailwind theme.
+                </Text>
+                <CodePreview
+                  language="javascript"
+                  textClass="code-snippet"
+                  text={`theme: {\n    extend: { colors: Tokens.colors } \n}`}
+                />
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+          <Text css={{ marginTop: '12px' }}>
+            To learn more about how to import these generated files, visit our{' '}
+            <Link
+              isExternal
+              color="blue.500"
+              href="https://github.com/Mirrorful/mirrorful"
+            >
+              documentation here.
+            </Link>
+          </Text>
+        </ModalBody>
+
+        <ModalFooter>
+          <Button
+            onClick={() => {
+              onClose()
+            }}
+            colorScheme="blue"
+          >
+            Done
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  )
+}
---
+++ packages/mirrorful/editor/src/components/Onboarding/index.tsx
@@ -0,0 +1,104 @@
+
+import { Box, Heading, Text } from '@chakra-ui/react'
+import { generateDefaultColorShades } from 'components/ColorPalette/utils'
+import { useState } from 'react'
+import { defaultTypography } from 'store/migrations'
+import { TColorData } from 'types'
+import { OnboardingCard } from './OnboardingCard'
+import { OnboardingContainer } from './OnboardingContainer'
+import { ImportInstructions } from './pages/ImportInstructions'
+import { NamePrimary } from './pages/NamePrimary'
+import { OtherColors } from './pages/OtherColors'
+import { PickPrimary } from './pages/PickPrimary'
+import { ReviewPrimary } from './pages/ReviewPrimary'
+import { Welcome } from './pages/Welcome'
+
+export function Onboarding({
+  onFinishOnboarding,
+}: {
+  onFinishOnboarding: () => void
+}) {
+  const [primaryColor, setPrimaryColor] = useState<string>('#9F7AEA')
+  const [primaryName, setPrimaryName] = useState<string>('')
+  const [palette, setPalette] = useState<TColorData[]>([])
+
+  const [page, setPage] = useState<number>(0)
+
+  const handleExport = async (
+    primaryColorHex: string,
+    primaryColorName: string,
+    latestPalette: TColorData[]
+  ) => {
+    const colors: TColorData[] = [
+      {
+        name: primaryColorName,
+        baseColor: primaryColorHex,
+        variants: generateDefaultColorShades(primaryColorHex),
+      },
+      ...latestPalette,
+    ]
+
+    await fetch('/api/export', {
+      method: 'POST',
+      body: JSON.stringify({
+        colorData: colors,
+        typography: defaultTypography,
+      }),
+    })
+  }
+
+  let content = (
+    <Welcome onFinishOnboarding={onFinishOnboarding} onUpdatePage={setPage} />
+  )
+
+  if (page === 1) {
+    content = (
+      <PickPrimary
+        initialPrimary={primaryColor}
+        onUpdatePage={setPage}
+        onUpdatePrimaryColor={(newColor: string) => setPrimaryColor(newColor)}
+      />
+    )
+  } else if (page === 2) {
+    content = (
+      <NamePrimary
+        initialName={primaryName}
+        onUpdatePage={setPage}
+        primaryColor={primaryColor}
+        onUpdatePrimaryName={(newName: string) => setPrimaryName(newName)}
+      />
+    )
+  } else if (page === 3) {
+    content = (
+      <ReviewPrimary primaryColor={primaryColor} onUpdatePage={setPage} />
+    )
+  } else if (page === 4) {
+    content = (
+      <OtherColors
+        initialPalette={palette}
+        onUpdatePalette={(newPalette: TColorData[]) => {
+          newPalette.map((color) => {
+            if (color.baseColor) {
+              color.variants = generateDefaultColorShades(color.baseColor)
+            }
+          })
+          handleExport(primaryColor, primaryName, newPalette)
+          setPalette(newPalette)
+        }}
+        primaryColor={primaryColor}
+        primaryName={primaryName}
+        onUpdatePage={setPage}
+      />
+    )
+  } else if (page === 5) {
+    content = (
+      <ImportInstructions
+        primaryColor={primaryColor}
+        onUpdatePage={setPage}
+        onFinish={onFinishOnboarding}
+      />
+    )
+  }
+
+  return <OnboardingContainer>{content}</OnboardingContainer>
+}
