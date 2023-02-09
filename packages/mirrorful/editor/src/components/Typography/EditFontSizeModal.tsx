57a7468566c2797de1740f9842dcb67523330cd1
397days <122672783+397days@users.noreply.github.com>
2023-02-09 07:35:55 +0000

    Update License.txt

--- packages/mirrorful/editor/src/components/Typography/EditFontSizeModal.tsx
+++ packages/mirrorful/editor/src/components/Typography/EditFontSizeModal.tsx
@@ -36,4 +36,108 @@ export function EditFontSizeModal({
   const {
     isOpen: isAlertDialogOpen,
     onOpen: onDeleteAlertDialogOpen,
-    on
+    onClose: onDeleteAlertDialogClose,
+  } = useDisclosure()
+
+  const [variant, setVariant] = useState<TFontSizeVariant>(
+    initialFontSizeVariant ?? { name: '', value: 1, unit: 'rem' }
+  )
+
+  const [error, setError] = useState<string | null>(null)
+
+  const handleSave = () => {
+    setError(null)
+    if (variant.name === '') {
+      setError('Please fill out all fields.')
+      return
+    }
+
+    if (variant.value !== 0 && !variant.value) {
+      setError('Please fill out all fields.')
+      return
+    }
+
+    onUpdateFontSizeVariant(variant)
+    onClose()
+  }
+
+  useEffect(() => {
+    if (!isOpen) {
+      setVariant(initialFontSizeVariant ?? { name: '', value: 1, unit: 'rem' })
+      setError(null)
+    }
+  }, [isOpen, initialFontSizeVariant])
+
+  return (
+    <>
+      <Modal isOpen={isOpen} onClose={onClose} size="xl">
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Edit Font Size Variant</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody
+            css={{
+              padding: '0px 32px 32px 32px',
+            }}
+          >
+            <Box css={{ display: 'flex', flexDirection: 'column' }}>
+              <FormControl>
+                <FormLabel>Variant Name</FormLabel>
+                <Input
+                  value={variant.name}
+                  onChange={(e) =>
+                    setVariant({ ...variant, name: e.target.value })
+                  }
+                />
+              </FormControl>
+
+              <FormControl css={{ marginTop: '32px' }}>
+                <FormLabel>Variant Value</FormLabel>
+                <Input
+                  value={variant.value}
+                  onChange={(e) =>
+                    setVariant({ ...variant, value: Number(e.target.value) })
+                  }
+                  type="number"
+                />
+              </FormControl>
+              <FormControl css={{ marginTop: '32px' }}>
+                <FormLabel>Variant Unit</FormLabel>
+                <Select
+                  value={variant.unit}
+                  onChange={(event) => {
+                    setVariant({
+                      ...variant,
+                      unit: event.target.value as 'px' | 'rem' | 'em',
+                    })
+                  }}
+                >
+                  <option value="px">px</option>
+                  <option value="rem">rem</option>
+                  <option value="em">em</option>
+                </Select>
+              </FormControl>
+            </Box>
+            {error && (
+              <Text color="red.500" css={{ marginTop: 18 }}>
+                {error}
+              </Text>
+            )}
+          </ModalBody>
+          <ModalFooter>
+            <Button
+              onClick={handleSave}
+              css={{ marginRight: '12px' }}
+              colorScheme="green"
+            >
+              Save Variant
+            </Button>
+            <Button onClick={() => onDeleteAlertDialogOpen()} colorScheme="red">
+              Delete Variant
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+      <AlertDialogDelete
+        tokenName={variant.name}
+        isOpe
